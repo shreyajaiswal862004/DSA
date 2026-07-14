5933e03a432c0c62b1fcaf8ae249137cf19b498b
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-07-14 20:19:06 +0530

    Time: 0 ms (100%), Space: 18.4 MB (32.29%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+
+        int inicolor=image[sr][sc];
+        int n=image.size();
+        int m=image[0].size();
+        if (inicolor == color) return image;
+        vector<vector<bool>>visited(n,vector<bool>(m,false));
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        visited[sr][sc]=true;
+        image[sr][sc]=color;
+        int delrow[]={0,-1,0,1};
+        int delcol[]={-1,0,1,0};
+
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+
+            q.pop();
+
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+
+                if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && image[nrow][ncol]==inicolor && !visited[nrow][ncol]){
+                    image[nrow][ncol]=color;
+                    q.push({nrow,ncol});
+                    visited[nrow][ncol]=true;
+                }
+            }
+        }
+        return image;
+    }
+};
