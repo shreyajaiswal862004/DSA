6be85f212d50111e86fddc7c09b39bead91d0dc9
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-07-25 00:43:20 +0530

    Time: 0 ms (100%), Space: 8.3 MB (40.62%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
     bool checkZeroOnes(string s) {
-        char ch=s[0];
-        int cnt1=0, cnt0=0, maxcount1=0, maxcount0=0;
-
-        if(s[0]=='1'){
-            cnt1++;
-            maxcount1=max(maxcount1,cnt1);
-        }
-        if(s[0]=='0'){
-            cnt0++;
-            maxcount0=max(maxcount0,cnt0);
-        }
-        for(int i=1;i<s.size();i++){
-            if(s[i]==ch){
-                if(s[i]=='1'){
-                    cnt1++;
-                    maxcount1=max(maxcount1,cnt1);
-                }
-                else{
-                    cnt0++;
-                    maxcount0=max(maxcount0,cnt0);
-                }
+        int cnt=1, maxcount0=0, maxcount1=0;
+        for(int i=0;i<s.size();i++){
+            if(i>0 && s[i]==s[i-1]){
+                cnt++;
+            }
+            else cnt=1;
+            if(s[i]=='1'){
+                maxcount1=max(maxcount1,cnt);
             }
             else{
-                ch=s[i];
-                if(s[i]=='1'){
-                    cnt1=1;
-                    maxcount1=max(maxcount1,cnt1);
-                }
-                else cnt0=1;{
-                    cnt0=1;
-                    maxcount0=max(maxcount0,cnt0);
-                }
+                maxcount0=max(maxcount0,cnt);
             }
         }
         return maxcount1>maxcount0;
