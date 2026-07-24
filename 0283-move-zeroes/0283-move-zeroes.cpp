01d2e1929b465f42f2a5091ffdd98357addae670
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-07-25 00:05:17 +0530

    Time: O(n), Space: O(1)

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        int j=-1;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                j=i;
-                break;
-            }
-        }
-
-        if(j == -1) return;
-
-        for(int i=j+1;i<n;i++){
-            if(nums[i]!=0){
-                swap(nums[i],nums[j]);
-                j++;
+        int i = 0;  // next position for non-zero
+        
+        for(int j = 0; j < nums.size(); j++){
+            if(nums[j] != 0){
+                swap(nums[i], nums[j]);
+                i++;
             }
         }
     }
