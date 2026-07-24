65b007a54914d08a665a4787f3ab68bb4e29182a
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-07-25 01:24:34 +0530

    Time: 79 ms (51.3%), Space: 146.4 MB (26.23%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>mpp;
+        mpp[0]=-1;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            int rem=sum%k;
+            if(mpp.find(rem)!=mpp.end()){
+                if(i-mpp[rem]>=2){
+                    return true;
+                }
+            }
+            else{
+                mpp[rem]=i;
+            }
+            
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
