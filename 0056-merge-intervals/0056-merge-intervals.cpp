04539823ee7ee0d86b9934267c6c5ce13c58ded5
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-07-30 23:46:28 +0530

    Time: 8 ms (23.69%), Space: 23.9 MB (30.99%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(ans.back()[1] >= intervals[i][0]){
+                ans.back()[1]=max(ans.back()[1], intervals[i][1]);
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
