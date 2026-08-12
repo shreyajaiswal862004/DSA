847bec397e4af8278fd2ad9e140bdb424c84ac14
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-08-12 17:54:10 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 701343-next-greater-element-GFG/701343-next-greater-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
+        stack<int>st;
+        vector<int>ans;
+        for(int i=arr.size()-1; i>=0;i--){
+            while(!st.empty() && st.top()<=arr[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(st.top());
+            }
+            st.push(arr[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
