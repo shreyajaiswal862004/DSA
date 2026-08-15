28ff423e6a4e1f2cd3da9d35739e0da4a38a704f
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-08-15 21:37:07 +0530

    Time: 3 ms (15.86%), Space: 27.4 MB (16.95%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,33 +1,22 @@
 class Solution {
-private:
-vector<int>pre(vector<int>&height, int n){
-    vector<int>prefix(n);
-    prefix[0]=height[0];
-    for(int i=1;i<height.size();i++){
-        prefix[i]=max(prefix[i-1],height[i]);
-    }
-    return prefix;
-}
-
-vector<int>suf(vector<int>&height, int n){
-    vector<int>suffix(n);
-    suffix[n-1]=height[n-1];
-    for(int i=n-2;i>=0;i--){
-        suffix[i]=max(suffix[i+1],height[i]);
-    }
-    return suffix;
-}
-
-
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int>prefix;
-        vector<int>suffix;
-        prefix=pre(height,n);
-        suffix=suf(height,n);
+        vector<int>prefix(n,0);
+        vector<int>suffix(n,0);
+
+        prefix[0]=height[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=max(height[i],prefix[i-1]);
+        }
+
+        suffix[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            suffix[i]=max(height[i], suffix[i+1]);
+        }
+
         int total=0;
-        for(int i=0;i<height.size();i++){
+        for(int i=0;i<n;i++){
             if(height[i]<prefix[i] && height[i]<suffix[i]){
                 total+=(min(prefix[i],suffix[i])-height[i]);
             }
