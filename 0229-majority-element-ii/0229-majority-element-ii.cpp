3e154ad72893ad5e85784c3c0533f635c45f4d75
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-08-16 00:57:19 +0530

    Time: 0 ms (100%), Space: 23.8 MB (44.79%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int el1=INT_MIN, el2=INT_MIN;
-        int cnt1=0,cnt2=0;
+        int el1, el2;
+        int count1=0, count2=0;
         vector<int>ans;
-
         for(int i=0;i<nums.size();i++){
-            if(cnt1==0 && nums[i]!=el2){
+            if(count1==0 && nums[i]!=el2){
                 el1=nums[i];
-                cnt1++;
+                count1++;
             }
-            else if(cnt2==0 && nums[i]!=el1){
+            else if(count2==0 && nums[i]!=el1){
                 el2=nums[i];
-                cnt2++;
+                count2++;
             }
-            else if(nums[i]==el1) cnt1++;
-            else if(nums[i]==el2) cnt2++;
-            else{
-                cnt1--;
-                cnt2--;
+            else if(el1==nums[i]) count1++;
+            else if(el2==nums[i]) count2++;
+            else {
+                count1--;
+                count2--;
             }
         }
 
-        cnt1=0, cnt2=0;
+        int cnt1=0, cnt2=0;
         for(int i=0;i<nums.size();i++){
             if(el1==nums[i]) cnt1++;
+        }
+        for(int i=0;i<nums.size();i++){
             if(el2==nums[i]) cnt2++;
         }
 
-        if(cnt1>nums.size()/3){
-            ans.push_back(el1);
-        }
-        if(cnt2>nums.size()/3){
-            ans.push_back(el2);
-        }
-        sort(ans.begin(),ans.end());
+        if(cnt1>(nums.size()/3)) ans.push_back(el1);
+        if(cnt2>(nums.size()/3)) ans.push_back(el2);
+
         return ans;
     }
 };
