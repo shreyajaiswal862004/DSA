92f2f3da1808f04bb34003e08a47fdf49844aed9
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-08-24 01:51:45 +0530

    Time: 0 ms (100%), Space: 14.2 MB (11.74%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[low]<=nums[high]){
-                ans=nums[low];
+                ans=min(ans,nums[low]);
                 break;
             };
             if(nums[low]<=nums[mid]){
