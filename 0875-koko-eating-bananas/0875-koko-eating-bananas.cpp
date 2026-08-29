4ec0ac1e02fb805fea1d32f020c2be44dab7d053
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-08-30 03:30:57 +0530

    Time: 7 ms (69.53%), Space: 23 MB (58.08%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+long long reqhrs(int mid, vector<int>&piles){
+    long long totalhrs=0;
+    for(int i=0;i<piles.size();i++){
+        totalhrs+=(piles[i]+mid-1)/mid;
+    }
+    return totalhrs;
+}
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long maxi=*max_element(piles.begin(),piles.end());
+
+        long long low=1, high=maxi, ans;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            long long hrs=reqhrs(mid,piles);
+
+            if(hrs<=h){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
