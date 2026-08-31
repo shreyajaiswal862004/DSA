b71182e785ba80c8fc74daf14d16d13e6c6e8ed4
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-08-31 19:27:16 +0530

    Time: 20 ms (68.64%), Space: 70.6 MB (48.75%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,48 +1,53 @@
 class Solution {
-private:
-    int isPossible(int mid, vector<int>& nums, int m, int k ){
-        int cnt=0;
-        int noOfbouq=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<=mid){
-                cnt++;
-            }
-            else{
-                noOfbouq+=(cnt/k);
-                cnt=0;
-            }
-        }
-        //last element cnt is counted in else loop
-        noOfbouq+=(cnt/k);
-        return noOfbouq>=m;
-    }
 public:
-    int minDays(vector<int>& nums, int m, int k) {
-        int n=nums.size();
-        int mini=INT_MAX;
+int isPossible(int mid, vector<int>&bloomDay, int k, int m){
+    int cnt=0, noOfbouq=0;
+
+    for(int i=0;i<bloomDay.size();i++){
+        if(bloomDay[i]<=mid){
+            cnt++;
+        }
+        else{
+            noOfbouq+=(cnt/k);
+            cnt=0;
+        }
+    }
+    noOfbouq+=(cnt/k);
+    return noOfbouq>=m;
+}
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n=bloomDay.size();
         int maxi=INT_MIN;
+        int mini=INT_MAX;
         int ans=-1;
-        //edge case if no. of bouq to make asked cant be made because of insufficient flower in array;
         long long val = 1LL*m*k;
         if(val>n) return -1;
-        for(int i=0;i<n;i++){
-            mini=min(mini, nums[i]);
-            maxi=max(maxi, nums[i]);
+
+        for(int i=0;i<bloomDay.size();i++){
+            if(mini>bloomDay[i]){
+                mini=bloomDay[i];
+            }
+            if(maxi<bloomDay[i]){
+                maxi=bloomDay[i];
+            }
         }
 
-        int low=mini;
-        int high=maxi;
+        int low=mini, high=maxi;
         while(low<=high){
-            int mid=low+(high-low)/2;
-
-            if(isPossible(mid, nums, m, k)){
+            int mid = low + (high-low)/2;
+            if(isPossible(mid,bloomDay,k,m)){
                 ans=mid;
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
+            
         }
         return ans;
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
