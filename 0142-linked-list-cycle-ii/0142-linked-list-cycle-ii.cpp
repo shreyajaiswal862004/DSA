83665bff9140a22d96559d872628f589937ed303
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-02 01:26:32 +0530

    Time: 10 ms (44.42%), Space: 11.4 MB (54.87%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -16,7 +16,6 @@ public:
             fast=fast->next->next;
             if(slow==fast) break;
         }
-
         if(fast==NULL || fast->next==NULL) return NULL;
         slow=head;
         while(slow!=fast){
