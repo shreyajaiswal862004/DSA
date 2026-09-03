c2a836866340d28a0a88647102d79c3a9bc26d78
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-04 00:26:49 +0530

    Time: O(n), Space: O(1)

--- 700053-add-1-to-a-linked-list-number-GFG/700053-add-1-to-a-linked-list-number.cpp
+++ 700053-add-1-to-a-linked-list-number-GFG/700053-add-1-to-a-linked-list-number.cpp
@@ -12,38 +12,45 @@ public:
 */
 class Solution {
   public:
-  Node* reverse(Node* head){
-      Node* back=NULL;
-      Node* temp=head;
-      
-      while(temp!=NULL){
-          Node* front=temp->next;
-          temp->next=back;
-          back=temp;
-          temp=front;
-      }
-      return back;
-  }
+  
+    Node* reverse(Node* head){
+        Node* back=NULL;
+        Node* temp=head;
+        while(temp!=NULL){
+            Node* front=temp->next;
+            temp->next=back;
+            back=temp;
+            temp=front;
+        }
+        return back;
+    }
+ 
     Node* addOne(Node* head) {
         // code here
-        head=reverse(head);
         
-        Node* curr = head;
+        if(head==NULL) return NULL;
+        Node* temp= reverse(head);
         int carry=1;
-        while(curr!=NULL && carry!=0){
-            int sum=curr->data+carry;
-            curr->data=sum%10;
-            carry=sum/10;
+        Node* dummy=new Node(-1);
+        Node* curr=temp;
+        dummy->next=curr;
+        
+        while(curr!=NULL || carry){
+            int sum=carry;
+            if(curr!=NULL){
+                sum+=curr->data;
+            }
             
-            if(carry && curr->next==NULL){
-                curr->next=new Node(1);
+            curr->data= (sum%10);
+            carry=sum/10;
+            if(curr->next == NULL && carry){
+                curr->next=new Node(carry);
                 carry=0;
             }
             curr=curr->next;
         }
         
-        head=reverse(head);
-        return head;
+        return reverse(temp);
         
     }
 };
