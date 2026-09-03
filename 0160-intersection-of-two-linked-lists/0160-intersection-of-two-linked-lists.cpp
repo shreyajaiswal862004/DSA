6af14f8ec59e26b93bf7f4f81db02b1a1362a912
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-03 21:30:32 +0530

    Time: 59 ms (40.38%), Space: 23.9 MB (55.75%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -44,10 +44,6 @@ public:
         while(tempA!=tempB){
             tempA=tempA->next;
             tempB=tempB->next;
-
-            if(tempA==tempB) return tempA;
-            if(tempA==NULL) tempA=headB;
-            if(tempB==NULL) tempB=headA;
         }
 
         return tempA;
