511a24a77d7b1d57b51e041323202f1a6d8b0e81
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-06 20:35:30 +0530

    Time: 7 ms (64.97%), Space: 11 MB (81.57%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int cnt=0;
+        int lastseen[3]={-1,-1,-1};
+        for(int i=0;i<s.size();i++){
+            lastseen[s[i]-'a']=i;
+            if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1){
+                cnt=cnt+(1+min({lastseen[0], lastseen[1], lastseen[2]}));
+            }
+        }
+        return cnt;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
