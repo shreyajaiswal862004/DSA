4bc2681dd38479066fdbca172e49ee85aafdbb4b
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-13 03:39:24 +0530

    Time: 2 ms (35.08%), Space: 9.9 MB (52.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string temp;
-        for(auto it:s){
-            if(isalnum(it)){
-                temp+=tolower(it);
-            }
-        }
-
-       // temp=tolower(temp);
-       int l=0, r=temp.size()-1;
+        int start=0;
+        int end=s.size()-1;
         
-        while(l<=r){
-            if(temp[l]!=temp[r]) return false;
-            l++;
-            r--;
+        while(start<end){
+            while(start<end && !isalnum(s[start])) start++;
+            while(start<end && !isalnum(s[end])) end--;
+            if(tolower(s[start])!=tolower(s[end])) return false;
+            start++;
+            end--;
         }
         return true;
     }
 };
 
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+//isalnum-> digits/letters
