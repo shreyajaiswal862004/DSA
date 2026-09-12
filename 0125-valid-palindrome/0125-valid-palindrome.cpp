0535defa6f56b377d259f16e552fe1e716e50cad
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-13 03:41:36 +0530

    Time: 3 ms (27.93%), Space: 9.9 MB (52.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int start=0;
-        int end=s.size()-1;
-        
-        while(start<end){
-            while(start<end && !isalnum(s[start])) start++;
-            while(start<end && !isalnum(s[end])) end--;
-            if(tolower(s[start])!=tolower(s[end])) return false;
-            start++;
-            end--;
+        int l=0, r=s.size()-1;
+
+        while(l<r){
+            while(l<r && !isalnum(s[l])) l++;
+            while(l<r && !isalnum(s[r])) r--;
+            if(tolower(s[l])!=tolower(s[r])) return false;
+            l++;
+            r--;
         }
         return true;
     }
 };
 
-//isalnum-> digits/letters
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         int start=0;
+//         int end=s.size()-1;
+        
+//         while(start<end){
+//             while(start<end && !isalnum(s[start])) start++;
+//             while(start<end && !isalnum(s[end])) end--;
+//             if(tolower(s[start])!=tolower(s[end])) return false;
+//             start++;
+//             end--;
+//         }
+//         return true;
+//     }
+// };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
