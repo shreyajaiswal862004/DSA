c7f428fcb229a250d263ab13782eae59210beb94
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-14 21:22:59 +0530

    Time: O(2^n), Space: O(n)

---
+++ 712211--GFG/712211-.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool numsub(int idx, int n, vector<int>& nums, int target, int sum) {
+
+        if(sum>target) return false;
+        
+        if (idx == n) {
+            return sum == target;
+        }
+
+        // Take the current element
+        if (numsub(idx + 1, n, nums, target, sum + nums[idx])) {
+            return true;
+        }
+
+        // Don't take the current element
+        if (numsub(idx + 1, n, nums, target, sum)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool checkSubsequenceSum(vector<int>& arr, int k) {
+        return numsub(0, arr.size(), arr, k, 0);
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
