89c89229e3085939d15e603bd1ff8c99ac99dc14
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-21 22:55:20 +0530

    Time: O(n), Space: O(1)

---
+++ 700665-insert-at-middle-of-linked-list-GFG/700665-insert-at-middle-of-linked-list.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    Node* insertInMiddle(Node* head, int x) {
+
+        Node* newNode = new Node(x);
+
+        if (head == NULL)
+            return newNode;
+
+        Node* slow = head;
+        Node* fast = head;
+
+        while (fast->next != NULL && fast->next->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        newNode->next = slow->next;
+        slow->next = newNode;
+
+        return head;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
