02ef46e415ddc13f80c5f2c4ea6811f9c1323a13
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-21 19:54:20 +0530

    Time: O(N), Space: O(1)

---
+++ 700039-length-of-linked-list-GFG/700039-length-of-linked-list.cpp
@@ -0,0 +1,29 @@
+/* Structure of linked list Node
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    int getCount(Node* head) {
+        // Code here
+        int cnt=0;
+        Node* temp=head;
+        while(temp!=NULL){
+            cnt++;
+            temp=temp->next;
+        }
+        return cnt;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
