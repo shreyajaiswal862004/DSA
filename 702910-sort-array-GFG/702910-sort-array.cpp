d6e6514bfdfb1b9a8b3b45d49b345e93ed0f4155
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-21 06:07:53 +0530

    Time: O(N log N), Space: O(1)

---
+++ 702910-sort-array-GFG/702910-sort-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void sortArr(vector<int> &arr) {
+        // code here
+        sort(arr.begin(),arr.end());
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
