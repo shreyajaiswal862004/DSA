1f4b694575514f9f17efb04a76e883bbdba28efd
shreyajaiswal862004 <shreyajaiswal08062004@gmail.com>
2026-09-26 00:56:40 +0530

    Time: O((N+M) log(N+M)), Space: O(N+M)

--- 702674-merge-and-sort-GFG/702674-merge-and-sort.cpp
+++ 702674-merge-and-sort-GFG/702674-merge-and-sort.cpp
@@ -3,29 +3,15 @@ class Solution {
     vector<int> mergeNsort(vector<int>& arr1, vector<int>& arr2) {
         //  code here
         vector<int>ans;
-        int n1=arr1.size();
-        int n2=arr2.size();
-        int i=0,j=0;
-        
-        while(i<n1){
-            ans.push_back(arr1[i]);
-            i++;
-        }
-        
-        while(j<n2){
-            ans.push_back(arr2[j]);
-            j++;
-        }
-        
-        sort(ans.begin(), ans.end());
         set<int>st;
-        for(auto it:ans){
-            st.insert(it);
-        }
-        ans.clear();
+        for(auto it:arr1) st.insert(it);
+        for(auto it:arr2) st.insert(it);
+        
         for(auto it:st){
             ans.push_back(it);
         }
+        
+        sort(ans.begin(), ans.end());
         return ans;
     }
 };
